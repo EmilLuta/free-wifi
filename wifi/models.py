d56f9fefd761c3895afda2bc2e900339acab76ce
devil <virgil.luta@gmail.com>
2020-06-20 10:11:03 +0200

    Add verified on password

--- wifi/models.py
+++ wifi/models.py
@@ -21,6 +21,7 @@ class Password(models.Model):
     location = models.ForeignKey('Local', on_delete=models.CASCADE)
     value = models.CharField(max_length=100)
     created_at = models.DateField(auto_now=True)
+    verified = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Password: {self.value} for location {self.location.name}'
--- wifi/views.py
+++ wifi/views.py
@@ -10,19 +10,30 @@ def index(request):
     return render(request, "index.html", {"locations": locations})
 
 
-class CreateLocal(CreateView, LoginRequiredMixin):
+class CreateLocal(LoginRequiredMixin, CreateView):
     model = Local
     fields = ('name', 'location')
     template_name = "create_local.html"
     success_url = "/"
 
 
-class CreatePassword(CreateView, LoginRequiredMixin):
+class CreatePassword(LoginRequiredMixin, CreateView):
     model = Password
     fields = ('value', 'location')
     template_name = "create_password.html"
     success_url = "/"
 
+    # def form_valid(self, form):
+        # import code
+        # code.interact(local=dict(globals(), **locals()))
+        # try:
+        #     user = User.objects.get(email=form.email)
+        # except User.DoesNotExist:
+        #     user = User.objects.create_user(form.email, form.email, ''.join(
+        #         [random.choice(string.digits + string.letters) for i in range(0, 10)]))
+        #     user.save()
+        # form.instance.user = user
+        # return super(ProjectCreateDetails, self).form_valid(form)
 
 def about(request):
     return render(request, "about.html")
